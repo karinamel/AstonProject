8f02292415788da77e4e90b20a8217b8b7db811d
Karina <kira4197@mail.com>
2024-10-09 13:06:24 +0300

    added bot states

---
+++ src/main/java/org/example/astonproject/enums/BotState.java
@@ -0,0 +1,7 @@
+package org.example.astonproject.enums;
+
+public enum BotState {
+    START,
+    WAITING_FOR_CITY
+}
+
--- src/main/java/org/example/astonproject/integrations/TelegramBot.java
+++ src/main/java/org/example/astonproject/integrations/TelegramBot.java
@@ -2,6 +2,7 @@ package org.example.astonproject.integrations;
 
 import org.example.astonproject.config.properties.TelegramBotProperties;
 import org.example.astonproject.dto.WeatherInfoDto;
+import org.example.astonproject.enums.BotState;
 import org.example.astonproject.services.WeatherInfoService;
 import org.springframework.stereotype.Component;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
@@ -9,18 +10,24 @@ import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Update;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.example.astonproject.enums.BotState.*;
+
 @Component
 public class TelegramBot extends TelegramLongPollingBot {
     private static final String WEATHER_MESSAGE_FORMAT = "Local temperature is %d Celsius, %s";
     private static final String START_MESSAGE = """
             Welcome to the Bot Assistant!
-            To find out the weather forecast, send a command "/weather Your city"
+            To find out the weather forecast, send a command /weather
             """;
-    private static final String EMPTY_CITY_MESSAGE = "Empty city. Send command \"/weather Your city\"";
+    private static final String ENTER_CITY_MESSAGE = "Enter the city";
     private static final String ERROR_MESSAGE = "There was an error sending the command. Check command.";
 
     private final TelegramBotProperties botProperties;
     private final WeatherInfoService weatherInfoService;
+    private final Map<Long, BotState> botStateMap = new HashMap<>();
 
     public TelegramBot(TelegramBotProperties telegramBotProperties, WeatherInfoService weatherInfoService) {
         super(telegramBotProperties.getToken());
@@ -36,41 +43,47 @@ public class TelegramBot extends TelegramLongPollingBot {
 
         String text = update.getMessage().getText();
         long chatId = update.getMessage().getChatId();
-        String command = text.contains(" ") ? text.substring(0, text.indexOf(" ")) : text;
 
-        switch (command) {
+        switch (text) {
             case "/start":
                 sendStartMessage(chatId);
                 break;
             case "/weather":
-                sendWeatherInfo(chatId, text);
+                sendWaitingCityMessage(chatId);
                 break;
             default:
+                if (botStateMap.get(chatId).equals(WAITING_FOR_CITY)) {
+                    sendWeatherInfo(chatId, text);
+                    return;
+                }
                 sendStartMessage(chatId);
         }
 
     }
 
-    private void sendWeatherInfo(long chatId, String text) {
-        String city = text.contains(" ") ? text.substring(text.indexOf(" ") + 1) : null;
-        if (city == null) {
-            sendMessage(chatId, EMPTY_CITY_MESSAGE);
-        } else {
-            try {
-                WeatherInfoDto weatherInfo = weatherInfoService.getWeatherInfo(city);
-                String message = WEATHER_MESSAGE_FORMAT.formatted(
-                        weatherInfo.getTemperature(), weatherInfo.getWeatherDescription().toLowerCase()
-                );
+    private void sendWeatherInfo(long chatId, String city) {
+        botStateMap.put(chatId, START);
 
-                sendMessage(chatId, message);
-            } catch (Exception e) {
-                sendMessage(chatId, ERROR_MESSAGE);
-            }
+        try {
+            WeatherInfoDto weatherInfo = weatherInfoService.getWeatherInfo(city);
+            String message = WEATHER_MESSAGE_FORMAT.formatted(
+                    weatherInfo.getTemperature(), weatherInfo.getWeatherDescription().toLowerCase()
+            );
+
+            sendMessage(chatId, message);
+        } catch (Exception e) {
+            sendMessage(chatId, ERROR_MESSAGE);
         }
     }
 
     private void sendStartMessage(long chatId) {
+        botStateMap.put(chatId, BotState.START);
         sendMessage(chatId, START_MESSAGE);
+    }
+
+    private void sendWaitingCityMessage(long chatId) {
+        botStateMap.put(chatId, WAITING_FOR_CITY);
+        sendMessage(chatId, ENTER_CITY_MESSAGE);
     }
 
     private void sendMessage(long chatId, String message) {
